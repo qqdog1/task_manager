e9c464a4317d5223436406174f8d0697da3b1f75
Shawn <chenhsiangchou@gmail.com>
2023-04-02 00:17:39 +0800

    增加點擊事件 修改觸控範圍

--- lib/page/daily_summary_page.dart
+++ lib/page/daily_summary_page.dart
@@ -35,26 +35,48 @@ class _DailySummaryPageState extends State<DailySummaryPage> {
       body: dataList.isEmpty
           ? const Center(child: CircularProgressIndicator())
           : ListView.builder(
-        itemCount: dataList.length,
-        itemBuilder: (context, index) {
-          final data = dataList[index];
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(data.name),
-                const SizedBox(height: 8),
-                LinearProgressIndicator(
-                  value: data.completed / data.total,
-                ),
-                const SizedBox(height: 8),
-                Text('${data.completed} / ${data.total}'),
-              ],
+              itemCount: dataList.length,
+              itemBuilder: (context, index) {
+                final data = dataList[index];
+                return GestureDetector(
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text('進度條被點擊了！ $index'),
+                          content: const Text('您可以在這裡執行您想要的操作'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text('關閉'),
+                            ),
+                          ],
+                        ));
+                  },
+                  child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    color: Colors.grey[200],
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(data.name),
+                            const SizedBox(height: 8),
+                            LinearProgressIndicator(
+                              value: data.completed / data.total,
+                            ),
+                            const SizedBox(height: 8),
+                            Text('${data.completed} / ${data.total}'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
